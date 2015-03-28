0c47558bd911e340479e79dd731484b45f93174e
etp12 <etp12@pitt.edu>
2015-03-27 23:39:45 -0400

    Update

--- selectfighter.py
+++ selectfighter.py
@@ -6,7 +6,7 @@ class SelectFighter(webapp2.RequestHandler):
 		self.response.out.write('User id not specified')
 	def post(self):
 		uid = self.request.get('uid')
-		common.render_template(self, 'main.html', {
+		common.render_template(self, 'selectFighter.html', {
 		'uid': uid
 		})
 		
