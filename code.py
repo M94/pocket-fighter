4d70c5e9b1d61404b332617ea0915da0248fd9c3
M94 <auc5@live.com>
2015-03-27 21:22:19 -0400

    Handlers
    Layed out all the handlers in the made code python file.

--- code.py
+++ code.py
@@ -15,6 +15,16 @@ class MainPage(webapp2.RequestHandler):
 		'welcome_msg':welcome_msg
 		})
 		
+class ProcessFighter(webapp2.RequestHandler)
+	def post(self):
+		fighter_name = self.request.get('fighter_name')
+		
 app = webapp2.WSGIApplication([
 	('/', MainPage),
+	('selectfighter', SelectFighter), # Handles the fighter select screen
+	('processfighter', ProcessFighter), # Handles the process for creating a fighter
+	('waitmatch', WaitMatch), # Handles the match waiting screen
+	('findmatch', FindMatch), # Handles the process of finding a match
+	('fight', Fight), # Handles the battle screen
+	('processfight', ProcessFight), # Handles all the updates for a battle
 	], debug=True)
